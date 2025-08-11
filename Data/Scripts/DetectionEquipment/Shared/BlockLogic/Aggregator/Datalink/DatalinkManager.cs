ff2fc2c2de83ee16256b1fc0771b67b118389c85
Aristeas <94058548+ari-steas@users.noreply.github.com>
2025-08-11 14:05:19 -0500

    add datalink antenna caching

--- Data/Scripts/DetectionEquipment/Shared/BlockLogic/Aggregator/Datalink/DatalinkManager.cs
+++ Data/Scripts/DetectionEquipment/Shared/BlockLogic/Aggregator/Datalink/DatalinkManager.cs
@@ -4,40 +4,104 @@ using Sandbox.Game.Entities;
 using Sandbox.ModAPI;
 using System.Collections.Generic;
 using VRage.Game.ModAPI;
+using VRage.Game.ModAPI.Ingame;
+using IMyCubeBlock = VRage.Game.ModAPI.IMyCubeBlock;
+using IMyCubeGrid = VRage.Game.ModAPI.IMyCubeGrid;
+using IMySlimBlock = VRage.Game.ModAPI.IMySlimBlock;
 
 namespace DetectionEquipment.Shared.BlockLogic.Aggregator.Datalink
 {
     internal static class DatalinkManager
     {
         public static Dictionary<IMyCubeGrid, Dictionary<int, HashSet<AggregatorBlock>>> ActiveDatalinkChannels;
+        private static Dictionary<IMyCubeGrid, HashSet<IMyFunctionalBlock>> _antennaCache;
 
         public static void Load()
         {
             ActiveDatalinkChannels = new Dictionary<IMyCubeGrid, Dictionary<int, HashSet<AggregatorBlock>>>();
+            _antennaCache = new Dictionary<IMyCubeGrid, HashSet<IMyFunctionalBlock>>();
+
+            MyAPIGateway.Entities.OnEntityAdd += OnEntityAdd;
+            MyAPIGateway.Entities.GetEntities(null, e =>
+            {
+                if (e is IMyCubeGrid)
+                    OnEntityAdd(e);
+                return false;
+            });
+
             Log.Info("DatalinkManager", "Loaded.");
         }
 
         public static void Unload()
         {
+            MyAPIGateway.Entities.OnEntityAdd -= OnEntityAdd;
+
+            _antennaCache = null;
             ActiveDatalinkChannels = null;
             Log.Info("DatalinkManager", "Unloaded.");
         }
 
+        private static void OnEntityAdd(IMyEntity e)
+        {
+            var grid = e as IMyCubeGrid;
+            if (grid == null)
+                return;
+            _antennaCache.Add(grid, new HashSet<IMyFunctionalBlock>());
+            grid.OnClosing += OnGridClose;
+            grid.OnBlockAdded += OnBlockAdded;
+            grid.OnBlockRemoved += OnBlockRemoved;
+            foreach (var block in grid.GetFatBlocks<IMyFunctionalBlock>())
+                OnBlockAdded(block.SlimBlock);
+        }
 
+        private static void OnBlockAdded(IMySlimBlock b)
+        {
+            var functional = b.FatBlock as IMyFunctionalBlock;
+            if (functional == null || !(functional is IMyRadioAntenna || functional is IMyLaserAntenna))
+                return;
+            HashSet<IMyFunctionalBlock> set;
+            if (!_antennaCache.TryGetValue(functional.CubeGrid, out set))
+                return; // either grid is dead or something has gone wrong
+            set.Add(functional);
+        }
+
+        private static void OnBlockRemoved(IMySlimBlock b)
+        {
+            var functional = b.FatBlock as IMyFunctionalBlock;
+            if (functional == null || !(functional is IMyRadioAntenna || functional is IMyLaserAntenna))
+                return;
+            HashSet<IMyFunctionalBlock> set;
+            if (!_antennaCache.TryGetValue(functional.CubeGrid, out set))
+                return; // either grid is dead or something has gone wrong
+            set.Remove(functional);
+        }
+
+        private static void OnGridClose(IMyEntity e)
+        {
+            _antennaCache.Remove((IMyCubeGrid)e);
+        }
 
         public static Dictionary<int, HashSet<AggregatorBlock>> GetActiveDatalinkChannels(IMyCubeGrid grid, long ownerIdentityId)
         {
             var receivers = new List<MyDataReceiver>();
-            foreach (var block in ((MyCubeGrid)grid).GetFatBlocks()) // TODO: Cache antennas.
-                if ((block is IMyRadioAntenna || block is IMyLaserAntenna) && ((IMyFunctionalBlock)block).Enabled)
-                    receivers.Add(block.Components.Get<MyDataReceiver>());
-
+      
             var connectedGrids = new HashSet<IMyCubeGrid>();
             grid.GetGridGroup(GridLinkTypeEnum.Logical).GetGrids(connectedGrids);
+            foreach (var cGrid in connectedGrids)
+            {
+                HashSet<IMyFunctionalBlock> antennaSet;
+                if (!_antennaCache.TryGetValue(cGrid, out antennaSet) || antennaSet.Count == 0)
+                    continue;
+                foreach (var block in antennaSet)
+                {
+                    if (block.Enabled)
+                        receivers.Add(block.Components.Get<MyDataReceiver>());
+                }
+            }
 
-            foreach (var reciever in TrackingUtils.GetAllRelayedBroadcasters(receivers, ownerIdentityId, false))
-                if (reciever.Entity is IMyCubeBlock)
-                    connectedGrids.Add(((IMyCubeBlock)reciever.Entity).CubeGrid);
+            foreach (var receiver in TrackingUtils.GetAllRelayedBroadcasters(receivers, ownerIdentityId, false))
+                if (receiver.Entity is IMyCubeBlock)
+                    connectedGrids.Add(((IMyCubeBlock)receiver.Entity).CubeGrid);
 
             var fullSet = new Dictionary<int, HashSet<AggregatorBlock>>();
 
